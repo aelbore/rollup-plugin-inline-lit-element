c3eb2698a69c93719e0a5a9aba7bd26c25d13e20
aelbore <aelbore.sg@gmail.com>
2020-02-07 18:46:58 +0800

    update submodules and scripts

--- tools/link.loader.ts
+++ tools/link.loader.ts
@@ -1,5 +1,9 @@
-import { symlinkDir } from 'aria-build'
+import { symlinkDir, exec } from 'aria-build'
 
 (async function() {
-  await symlinkDir('./dist', './node_modules/rollup-plugin-inline-lit-element')
+  await exec('npm --prefix ./transpiler run build')
+  await Promise.all([
+    symlinkDir('./dist', './node_modules/rollup-plugin-inline-lit-element'),
+    symlinkDir('./transpiler/dist', './node_modules/lit-element-transpiler')
+  ])
 })()
